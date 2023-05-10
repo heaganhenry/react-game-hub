c69dc343a6fdc516a6cfce339facf4406826e9a7
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2023-05-10 06:10:44 -0400

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.svg.png";
+
 const getCroppedImageURL = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
 
   const target = "media/";
   const index = url.indexOf(target) + target.length;
